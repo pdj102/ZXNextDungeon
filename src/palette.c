42903910b8163f08e08fd7bc2517ec7863c537b7
Paul Johnson <pdj102@yahoo.co.uk>
2021-10-03 21:56:05 +0100

    Moved palette data to asm file

--- src/palette.c
+++ src/palette.c
@@ -10,9 +10,15 @@
 #include <arch/zxn.h>
 #include "inttypes.h"
 
+// Set TILE_PALETTE_SIZE to the size of the palette data
+#define TILE_PALETTE_SIZE 96
+
 /***************************************************
  * private types
  ***************************************************/
+
+// tile_palette is defined in palette_data.asm
+extern uint8_t tile_palette[];
 
 /***************************************************
  * private function prototypes
@@ -21,66 +27,7 @@
 /***************************************************
  * private variables
  ***************************************************/
-// Tile map palette
-// RRRGGGBB 8 bit colour
-// NB Default transparency palette index is 0xF
-static const uint8_t tile_palette[] = 
-{
-// 0 - wall palette
-    0x24,       
-    0x29,       
-    0x44,       
-    0x41,       
-    0x6e,       
-    0x97,       
-    0xFF,       
-    0x0,       
-    0x0,
-    0x0,
-    0x0,    
-    0x0,
-    0x0,
-    0x0,    
-    0x0,
-    0x0,        // default transparent index
 
-// 1 player palette
-    0x24,
-    0x28,
-    0x2d,
-    0x48,
-    0x6d,
-    0xd5,
-    0xda,
-    0xfa,
-    0xff,
-    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-
-// 2 monster palette
-    0x24, 0x64, 0x6d, 0x89,
-	0xff, 0xd5, 0x0, 0x0,
-	0x0, 0x0, 0x0, 0x0,
-	0x0, 0x0, 0x0, 0x0,     
-
-// 3 not used - make a green item palette
-    0x0, 0x0, 0x0, 0x0,
-    0x0, 0x0, 0x0, 0x0,
-    0x0, 0x0, 0x0, 0x0,
-    0x0, 0x0, 0x0, 0x0,  
-
-// 4 red items
-	 0x0, 0xff, 0x8c, 0xb0,
-	 0xd5, 0x64, 0xa4, 0xc9,
-	 0x0, 0x0, 0x0, 0x0,
-	 0x0, 0x0, 0x0, 0x0,
-
-// 5 blue items
-	 0x0, 0xff, 0x8c, 0xb0,
-	 0xd5, 0x1, 0x3, 0x2f,
-	 0x0, 0x0, 0x0, 0x0,
-	 0x0, 0x0, 0x0, 0x0,
-
-};
 
 
 /***************************************************
@@ -99,7 +46,11 @@ void palette_init()
 
     // 041 Palette Value (8 bit colour)
     // Write palette
-    for (uint8_t i = 0 ; i < sizeof tile_palette / sizeof tile_palette[0]; i++) {
+    
+    // Tile map palette
+    // RRRGGGBB 8 bit colour
+    // NB Default transparency palette index is 0xF
+    for (uint8_t i = 0 ; i < TILE_PALETTE_SIZE; i++) {
         //ZXN_WRITE_REG(0x41,i);
         ZXN_WRITE_REG(0x41, tile_palette[i]);
     }
